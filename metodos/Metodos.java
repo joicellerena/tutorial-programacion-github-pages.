66cbe3607059abacfdada21c9e4d6566393b4da1
joicellerena <joicellerena9@gmail.com>
2026-06-14 00:07:51 -0500

    Add Metodos class with suma and multiplicar methods

---
+++ metodos/Metodos.java
@@ -0,0 +1,18 @@
+public class Metodos {
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Suma: " + suma(5, 3));
+        System.out.println("Multiplicación: " + multiplicar(5, 3));
+
+    }
+
+}
